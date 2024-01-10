a86f02fabc93a39fb92a5ce8539746302285e772
OSSU <o.ss.u2056@gmail.com>
2024-01-11 02:02:56 +0900

    chore: spring 초기 설정

---
+++ src/main/java/com/example/inkspire/InkspireBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.inkspire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkspireBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkspireBeApplication.class, args);
+    }
+
+}
