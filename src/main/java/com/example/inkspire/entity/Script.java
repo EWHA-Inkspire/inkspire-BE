9383749a9470871c5d6776fd1100e228f64b0772
OSSU <o.ss.u2056@gmail.com>
2024-01-20 13:31:23 +0900

    feat: Script 엔티티 생성 - character : 캐릭터 테이블 외래키로 참조, 대부분 캐릭터 - 스크립트 같이 사용할 예정이라 fetch를 EAGER로 설정함 - time, place : 스크립트의 시간적 공간적 배경 - universe : 스크립트 세계관 - ending : 진엔딩 달성 여부

---
+++ src/main/java/com/example/inkspire/entity/Script.java
@@ -0,0 +1,33 @@
+package com.example.inkspire.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Script {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "character_id", nullable = false)
+    private Character character;
+
+    @Column(name = "time", nullable = false)
+    private String time;
+
+    @Column(name = "place", nullable = false)
+    private String place;
+
+    @Column(name = "genre", nullable = false)
+    private String genre;
+
+    @Column(name = "universe")
+    private String universe;
+
+    @Column(name = "ending", nullable = false)
+    private boolean ending;
+}
