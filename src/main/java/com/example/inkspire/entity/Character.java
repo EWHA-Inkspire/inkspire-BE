cf56464e55fab1a44adbbef43b4abb15dfb36bd8
OSSU <o.ss.u2056@gmail.com>
2024-01-20 13:08:25 +0900

    feat: Character entity 생성 - member : 회원 테이블 외래키로 참조, 대부분 멤버 - 캐릭터 같이 사용할 예정이라 fetch를 EAGER로 설정함 - luck, defense, mental, agility, attack, hp : 캐릭터 스탯 - name : 캐릭터 이름 - success / fail : 성공 업적, 실패 업적

---
+++ src/main/java/com/example/inkspire/entity/Character.java
@@ -0,0 +1,45 @@
+package com.example.inkspire.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Character {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "luck", nullable = false)
+    private Integer luck;
+
+    @Column(name = "defense", nullable = false)
+    private Integer defense;
+
+    @Column(name = "mental", nullable = false)
+    private Integer mental;
+
+    @Column(name = "agility", nullable = false)
+    private Integer agility;
+
+    @Column(name = "attack", nullable = false)
+    private Integer attack;
+
+    @Column(name = "hp", nullable = false)
+    private Integer hp;
+
+    @Column(name = "success")
+    private String success;
+
+    @Column(name = "fail")
+    private String fail;
+}
