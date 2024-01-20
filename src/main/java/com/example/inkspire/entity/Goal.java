f53f46eec4b53ae1b146ee341ecfb1572a163bdc
OSSU <o.ss.u2056@gmail.com>
2024-01-20 13:39:14 +0900

    feat: goal 엔티티 생성 - script : 스크립트 테이블 외래키로 참조 - chapter : 어떤 챕터의 목표인지 - content, require, type : 목표 내용, 달성 조건, 목표 유형(아이템 획득, 진상 밝히기, 적 처치) - etc : 목표 유형 별 필요한 정보 저장 - achieved : 달성 여부

---
+++ src/main/java/com/example/inkspire/entity/Goal.java
@@ -0,0 +1,37 @@
+package com.example.inkspire.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Goal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "script_id", nullable = false)
+    private Script script;
+
+    @Column(name = "chapter", nullable = false)
+    private Integer chapter;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "require", nullable = false)
+    private String require;
+
+    // TODO: common_code 테이블로 관리할 사항 많아질 경우 수정하기
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "etc", nullable = false)
+    private String etc;
+
+    @Column(name = "achieved", nullable = false)
+    private boolean achieved;
+}
