40a61be79e3e1f1c0313397c61e31ad3522dabac
OSSU <o.ss.u2056@gmail.com>
2024-01-20 15:35:08 +0900

    feat: member repository interface 생성

---
+++ src/main/java/com/example/inkspire/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.inkspire.repository;
+
+import com.example.inkspire.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
