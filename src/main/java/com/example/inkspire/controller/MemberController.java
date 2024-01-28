43a39e4698dcfa067f5afd5da551535091a7c3d7
OSSU <o.ss.u2056@gmail.com>
2024-01-28 23:33:10 +0900

    feat: 프로필 조회 컨트롤러 생성 GET 방식으로 회원 정보 조회 요청이 들어올 경우 해당 서비스 로직에 memberId값 전달

--- src/main/java/com/example/inkspire/controller/MemberController.java
+++ src/main/java/com/example/inkspire/controller/MemberController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -29,5 +26,11 @@ public class MemberController {
     @PostMapping("/login")
     public ResponseEntity<DataResponseDto<Long>> login(@Validated @RequestBody Map<String, String> map) {
         return new ResponseEntity<>(memberService.join(map.get("email"), map.get("password")), HttpStatus.OK);
+    }
+
+    /* 프로필 정보 조회 */
+    @GetMapping("/profile")
+    public ResponseEntity<DataResponseDto<MemberInfoDto>> profile(@Validated @RequestParam Long id) {
+        return new ResponseEntity<>(memberService.getMemberInfo(id), HttpStatus.OK);
     }
 }
