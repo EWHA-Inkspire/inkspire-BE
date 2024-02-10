04c93a3e7527f8cbadca53c99616cd9fe4132120
OSSU <o.ss.u2056@gmail.com>
2024-02-10 14:14:45 +0900

    feat: npc 정보 저장 api 구현
    NpcInfoDto로 npc 정보를 받아와 repository에 저장되도록 구현함. 예외 처리는 추후 리팩토링 과정을 거치며 추가할 예정. 저장 성공시 NpcId값을 리턴하도록 설정함.

--- src/main/java/com/example/inkspire/entity/Map.java
+++ src/main/java/com/example/inkspire/entity/Map.java
@@ -1,12 +1,18 @@
 package com.example.inkspire.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Map {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
