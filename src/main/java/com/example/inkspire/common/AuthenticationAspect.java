d6a795e088b4229013ba51f81d69193d3a25f49c
OSSU <o.ss.u2056@gmail.com>
2024-02-11 14:21:58 +0900

    feat: 캐릭터 존재 여부 확인 로직 추가

--- src/main/java/com/example/inkspire/common/AuthenticationAspect.java
+++ src/main/java/com/example/inkspire/common/AuthenticationAspect.java
@@ -1,5 +1,6 @@
 package com.example.inkspire.common;
 
+import com.example.inkspire.character.CharacterRepository;
 import com.example.inkspire.common.errors.ErrorCode;
 import com.example.inkspire.common.errors.GeneralException;
 import com.example.inkspire.user.UserRepository;
@@ -13,16 +14,25 @@ import org.springframework.stereotype.Component;
 public class AuthenticationAspect {
 
     private final UserRepository userRepository;
+    private final CharacterRepository characterRepository;
 
     @Autowired
-    public AuthenticationAspect(UserRepository userRepository) {
+    public AuthenticationAspect(UserRepository userRepository, CharacterRepository characterRepository) {
         this.userRepository = userRepository;
+        this.characterRepository = characterRepository;
     }
 
-    @Before("@annotation(com.example.inkspire.common.annotation.RequiresAuthentication) && args(userId,..)")
-    public void authenticate(Long userId) {
+    @Before("@annotation(com.example.inkspire.common.annotation.UserAuthentication) && args(userId,..)")
+    public void authenticateUser(Long userId) {
         if(!userRepository.existsById(userId)) {
             throw new GeneralException(ErrorCode.USER_NOT_FOUND);
+        }
+    }
+
+    @Before("@annotation(com.example.inkspire.common.annotation.CharacterAuthentication) && args(characterId,..)")
+    public void authenticateCharacter(Long characterId) {
+        if (!characterRepository.existsById(characterId)) {
+            throw new GeneralException(ErrorCode.CHARACTER_NOT_FOUND);
         }
     }
 }
