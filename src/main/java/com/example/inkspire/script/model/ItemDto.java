d7eac905500794fe0e3310f90e4dd030c2e11eb8
OSSU <o.ss.u2056@gmail.com>
2024-02-11 15:41:01 +0900

    feat: 아이템 정보 dto 생성
    아이템이 위치할 장소 아이디와 아이템 이름, 아이템 상세 정보, 아이템 타입을 dto로 받아 옴

---
+++ src/main/java/com/example/inkspire/script/model/ItemDto.java
@@ -0,0 +1,23 @@
+package com.example.inkspire.script.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ItemDto {
+
+    @NotNull(message = "맵 아이디는 필수 입력 값입니다.")
+    private Long mapId;
+
+    @NotBlank(message = "아이템 이름은 필수 입력 값입니다.")
+    private String name;
+
+    private String detail;
+
+    @NotBlank(message = "아이탬 유형은 필수 입력 값입니다.")
+    private String type;
+
+}
